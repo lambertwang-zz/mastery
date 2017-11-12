8e62cabb6c44cf55d4d04e86a8549e5646fe51d1
Ben McMorran <ben.mcmorran@gmail.com>
2017-11-11 20:00:49 -0800

    Working conversations about monsters

--- src/util.py
+++ src/util.py
@@ -1,5 +1,6 @@
 import random
 import re
+import json
 
 def load_words(path):
     with open(path, 'r') as f:
@@ -41,6 +42,9 @@ class MarkovGenerator:
 town_generator = MarkovGenerator(load_words('../data/towns.txt'), 2)
 name_generator = MarkovGenerator(load_words('../data/names_male.txt'), 3)
 occupation_list = list(load_words('../data/occupations.txt'))
+
+with open('../monsters.json', 'r') as monster_file:
+    monsters_list = json.load(monster_file)
 
 def expand(sentence, **kwargs):
     while True:
@@ -177,14 +181,61 @@ def town_intro():
         random.choice([
             '"<!armor_name> is the best in town. His shop is <in_town_directions> ',
             '"Go <in_town_directions> You\'ll find <!armor_name>, the best weapons expert we\'ve got. ',
-            '"<!armor_name> is <in_town_directions>. Tell him I sent you. '
+            '"<!armor_name> is <in_town_directions> Tell him I sent you. '
         ]) +
         random.choice([
             'And here, take a few gold pieces to buy the best." He reached into his pocket and pulled out <number> small coins. '
-            'I want that <!wiz_name> gone as much as anybody."\n',
+            '"I want that <!wiz_name> gone as much as anybody."\n',
             'Be careful out there. You\'re not the first to try this adventure. Men stronger than you have vanished or worse."\n',
             'I\'d show you myself, but I have urgent matters to attend to here in the square."\n'
         ]) +
         '<!pc_name> hurried towards the armory. <town_people_sentence> <town_people_sentence> '
-        'Turning the corner, he saw the armory in front of him.'
+        'Turning the corner, he saw the armory in front of him. He pushed the door open and walked inside.\n'
     )
+
+def monster_name():
+    return random.choice([monster['name'].strip() for monster in monsters_list])
+
+def monster_description(name):
+    matches = [monster for monster in monsters_list if monster['name'].strip() == name]
+    if matches:
+        return matches[0]['description']
+    else:
+        return ['The monster ' + name + ' is terrifying for sure, but I honestly don\'t know much about that beast.']
+
+def armory_intro():
+    return (
+        random.choice([
+            '<!armor_name> looked up from his work behind a counter at <!pc_name>.\n',
+            'There was no one there. <!pc_name> cleared his throat and a man ran out from a backroom.\n'
+        ]) +
+        '"I\'m <!pc_name>, a brave adventurer seeking to destroy <!wiz_name>. What dangers lurk nearby?" he asked.\n' +
+        random.choice([
+            '<!armor_name> grabbed a dusty book from the shelf and flipped through it. Pictures of <monster_name>s and <monster_name>s flew by.'
+            'Eventually he settled on a page and started to explain.\n',
+            '<!armor_name> lifted up his tunic and pointed to a scar. "You see this?" he asked. "Only one monster can do this kind of damage. The <!monster_name>."\n',
+            '"Brave you say? You may have fought the <monster_name>, or perhaps even the <monster_name>, but that\'s nothing compared to the <!monster_name> we\'ve got."\n'
+        ])
+    )
+
+def armory_explanation():
+    return random.choice([
+        '"<!description>" <!armor_name> explained.\n',
+        'The armorer sighed and continued. "<!description>"\n',
+        '<!armor_name> returned to the book of monsters on the desk and pointed at the terrifying illustration. "<!description>"\n'
+    ])
+
+def armory_more():
+    return random.choice([
+        '<!pc_name> looked surprised. "Incredible! Is there anything else I should know?"\n',
+        '"But my weapons may be too weak. Are there any other ways to defeat the <!monster_name>?" <!pc_name> asked.\n',
+        '<!pc_name> slipped the man <number> coins. "I get the feeling you\'ve been here for a while. Surely you know more than that."\n',
+        '"I could handle that. Tell me again, what makes the <!monster_name> so bad?" <!pc_name> responded.\n'
+    ])
+
+def armory_no_more():
+    return random.choice([
+        '"That\'s all I can tell you."\n',
+        '"Anything else you need to know can be found it the book. Take your time." He took the book of monsters and handed it to <!pc_name>.\n',
+        '"Look I\'ve got other things to attend to. Do you need weapons or not?" His frusturation was visible.'
+    ])
